e2a34d1b92d60b4b3cabb19af644670364538246
Hossam Hilal <46751584+hossamhilal@users.noreply.github.com>
2022-06-29 13:33:42 +0300

    upload new files

--- js/custom.js
+++ js/custom.js
@@ -60,47 +60,6 @@
         }
     });
 
-    
-    // Show / Hide block img animation 
-    var position = $('.blocks').position();
-    console.log({position})
-    $(window).scroll( function(){
-        let sT =  $('.blocks').offset().top - 500,
-            wT =  $(this).scrollTop();
-        if ( wT > sT ){
-            $('.blockTitle').addClass('scrolled');
-          } else {
-            $('.blockTitle').removeClass('scrolled');
-          }
-    });
-
-    // Scroll To Top 
-    // $('.up').click( function() {
-    //     $('html, body').animate({ 
-    //         scrollTop: 0
-    //     }, 2000);
-    // });
-
-
-    // INPUT ANIMATION 
-    $('.filedInput').each(function() { 
-        if ($(this).val() != "") {
-            $(this).parent('.field').addClass('focused');   
-        }
-    });
-
-    $('.filedInput').focus(function(){
-        $(this).parent().addClass('focused');
-    });
-
-    $('.filedInput').focusout(function(){
-        if($(this).val() === "") $(this).parent('.field').removeClass('focused');
-    });
-
-    $('button[type="reset"]').on('click' , function(){
-        $('.field').removeClass('focused');
-    });
-
     // init wow 
     new WOW().init();
 
